a26943f66116aab4c85b82efc256cd89ea1a4830
cookie <cookdon3@gmail.com>
2019-04-09 13:26:40 +0900

    スクリプトを追加

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+git clone https://github.com/cook-cookie/dotfiles.git
+cd ~/dotfiles
+brew bundle
+
 DOT_FILES=(.zshrc .vimrc .vim)
 
 for file in ${DOT_FILES[@]}
