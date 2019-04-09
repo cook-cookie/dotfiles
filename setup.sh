77bb573a8e354138887de39de90d2fd0cb018c91
cookie <cookdon3@gmail.com>
2019-04-09 14:03:05 +0900

    やることメモを追加、スクリプトに追記

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+defaults write com.apple.finder AppleShowAllFiles TRUE
+killall Finder
+mkdir ~/Pictures/screenshots
+defaults write com.apple.screencapture location ~/Pictures/screenshots
+killall SystemUIServer
 xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install git
 git clone https://github.com/cook-cookie/dotfiles.git
 cd ~/dotfiles
-brew bundle
-
 DOT_FILES=(.zshrc .vimrc .vim)
-
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+brew bundle
+echo
